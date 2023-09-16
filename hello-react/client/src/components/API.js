5b01c0892572d50c6feb426abdbadf2eb4344cfa
Ruben <rubend.alvarezc@gmail.com>
2023-09-16 11:08:23 -0400

    Variable City and Pictures

--- hello-react/client/src/components/API.js
+++ hello-react/client/src/components/API.js
@@ -1,26 +1,23 @@
 import axios from 'axios';
 
-// const search = async (query) =>
-//   axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${query}&rating=pg`);
 
-// export default { search };
+async function property(props) {
+  const { city } = props.city;
 
-//const axios = require('axios');
-
-const apiFilter = {
-  method: 'GET',
-  url: 'https://airbnb-search.p.rapidapi.com/property/search',
-  params: {
-    query: 'Miami, FL'
-  },
-  headers: {
-    'X-RapidAPI-Key': 'ef0bd9f482msh63afeccd9f08d96p14e2f6jsn88cf7f9d5d03',
-    'X-RapidAPI-Host': 'airbnb-search.p.rapidapi.com'
-  }
-};
-
-async function property() {
   try {
+    console.log(city);
+    const apiFilter = {
+      method: 'GET',
+      url: 'https://airbnb-search.p.rapidapi.com/property/search',
+      params: {
+        query: city,
+      },
+      headers: {
+        'X-RapidAPI-Key': 'ef0bd9f482msh63afeccd9f08d96p14e2f6jsn88cf7f9d5d03',
+        'X-RapidAPI-Host': 'airbnb-search.p.rapidapi.com',
+      },
+    };
+
     const response = await axios.request(apiFilter);
     console.log(response.data);
     return response.data;
@@ -29,8 +26,7 @@ async function property() {
   }
 }
 
-export default { property };
-
+export default property;
 
 
 
--- hello-react/client/src/components/propertyDetail.js
+++ hello-react/client/src/components/propertyDetail.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 function PropertyDetail(props) {
-  
+  const result = props.list.slice(1);
+
   if (!props.list?.length) {
     return null;
-  } else { console.log(props.list)}
+  } else { 
+    console.log(result)}
 
   return (
     <div className="text-center">
-        {props.list.map((item, index) => (
+        {result.map((item, index) => (
           <ul>
             <li id={`nam-${index}`} key={`name-${index}`}>{item.listing.name}</li>
             <li id={`cit-${index}`} key={`city-${index}`}>{item.listing.city}</li>
